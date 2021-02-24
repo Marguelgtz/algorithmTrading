f126d1867a84cfe949977e5a7082a9b3b5496bbf
EC2 Default User <ec2-user@ip-172-31-83-254.ec2.internal>
2021-02-24 09:11:29 +0000

    access path and got appl.csv

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot  as plt
-plt.style.use('fivethirdyeight')
+plt.style.use('fivethirtyeight')
 
+import os
+script_path = os.path.abspath(__file__)
+script_dir = os.path.split(script_path)[0] 
+rel_path = "sample_data/appl.csv"
+abs_file_path = os.path.join(script_dir, rel_path)
 
+#Read file
+APPL_data = open(rel_path, 'r')
+
+#Store data 
+APPL_data = pd.read_csv('appl.csv')
 
