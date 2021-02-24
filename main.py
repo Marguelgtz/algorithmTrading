f934cc72db121464a54225d8f8cfcec727ef2119
EC2 Default User <ec2-user@ip-172-31-83-254.ec2.internal>
2021-02-24 08:20:42 +0000

    created main file and added libraries

---
+++ main.py
@@ -0,0 +1,12 @@
+#Desc: Dual Moving average cross over to determine transactions
+
+#libraries
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot  as plt
+plt.style.use('fivethirdyeight')
+
+
+
